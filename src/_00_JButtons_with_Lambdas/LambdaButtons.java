dce7066334e2d465c6284962cf8d28fe30eb5aa6
league <league@imac-18.attlocal.net>
2019-08-05 15:40:42 -0700

    done

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,9 +1,11 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class LambdaButtons {
 	private JFrame window = new JFrame();
@@ -20,6 +22,20 @@ public class LambdaButtons {
 		
 		//1. Call the addActionListener methods for each button. Use lambdas
 		//   to define to functionality of the buttons.
+		
+		addNumbers.addActionListener(e ->{
+			int a = Integer.parseInt(JOptionPane.showInputDialog("give a number"));
+			int b = Integer.parseInt(JOptionPane.showInputDialog("give a number"));
+			JOptionPane.showMessageDialog(null, a + " + " + b + " = " + (a + b));
+		});
+		
+		randNumber.addActionListener(e ->{
+			JOptionPane.showMessageDialog(null, "rand: " + new Random().nextInt(1000));
+		});
+		
+		tellAJoke.addActionListener(e ->{
+			JOptionPane.showMessageDialog(null, "knock, knock");
+		});
 		
 		window.setVisible(true);
 		window.pack();
--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -13,13 +13,35 @@ public class LambdaMethods {
 		}, "repeat");
 		
 		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
-		
+		printCustomMessage((s)->{
+			String r = "";
+			for (int i = s.length() - 1; i >= 0; i--)
+			{
+				r+=s.charAt(i);
+			}
+			System.out.println(r);
+		} , "backwards");
 		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
-		
+		printCustomMessage((s)->{
+			String r = "";
+			for (int i = 0; i < s.length(); i++)
+			{
+				r += (i%2 == 0) ? s.toUpperCase().charAt(i) : s.toLowerCase().charAt(i);
+			}
+			System.out.println(r);
+		}, "upperlowermix");
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
-		
+		printCustomMessage((s)->{
+			String r = "" + s.charAt(0);
+			for(int i = 1; i < s.length(); i++) {
+				r+="." + s.charAt(i);
+			}
+			System.out.println(r);
+		}, "periods");
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
-	
+		printCustomMessage((s)->{
+			System.out.println(s.replaceAll("[aeiou]", ""));
+		}, "no vowels anymore");
 	}
 	
 	public static void printCustomMessage(SpecialPrinter sp, String value) {
